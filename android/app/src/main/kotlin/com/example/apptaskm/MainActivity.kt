b8291657fd8d35aa4799aaab6369da6678bb265b
100450 <100450@MACTECH.LOC>
2024-10-19 11:42:07 +0530

    project completed

---
+++ android/app/src/main/kotlin/com/example/apptaskm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.apptaskm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:apptaskm/screens/login_screen.dart';
+import 'package:apptaskm/screens/mainscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'todo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/main': (context) => MainScreen(),
+      },
+    );
+  }
+}
